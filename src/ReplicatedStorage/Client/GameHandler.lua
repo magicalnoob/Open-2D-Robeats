48d7acc73b407f58e9b94ab09279ca88f2d44697
magicalnoob <61070617+magicalnoob@users.noreply.github.com>
2020-11-10 22:01:20 -0800

    Fixed a bug where sliders will not appear
    Im stupid and i made duration lowercase LOL

--- src/ReplicatedStorage/Client/GameHandler.lua
+++ src/ReplicatedStorage/Client/GameHandler.lua
@@ -1,251 +1,152 @@
---- GameHandler.lua
--- Manage game state bois
+--- Slider.lua
+-- A simple slider
 -- @author Magical_Noob
 
-local SONGS_FOLDER = game.ReplicatedStorage.Songs;
+local Gameplay = script.Parent.Parent;
+local Config = require(game.ReplicatedStorage:WaitForChild("GameConfig"));
+local Signal = require(Gameplay:WaitForChild("Signal"));
 
---- Really bad
-local UserInputService = game:GetService("UserInputService");
-local Config = require(game.ReplicatedStorage.GameConfig);
-local SoundHandler = require(script.Parent.SoundHandler);
-local Gameplay = script.Parent.Gameplay;
-local FastSpawn = require(Gameplay.FastSpawn);
-local FastWait = require(Gameplay.FastWait);
-local Signal = require(Gameplay.Signal);
-local Track = require(Gameplay.Track);
+local Slider = {};
+Slider.__index = Slider;
 
-local FORMAT = "%0.1f";
-local GameHandler = {};
-GameHandler.__index = GameHandler;
-GameHandler.BackgroundGui = nil;
-GameHandler.NotesGui = nil;
+local ENUMS = {
+	failed = 0;
+	passed = 1;
+}
 
-GameHandler.BackgroundMusic = Instance.new("Sound");
-GameHandler.Add = 0;
-GameHandler.Max = 0;
-GameHandler.Combo = 0;
-GameHandler.Misses = 0;
-
-GameHandler.ScoreChanged = Signal:new();
-GameHandler.DisplayText = Signal:new();
-
-GameHandler.gameTime = 0;
-GameHandler.timeScale = 0;
-GameHandler.speed = 0.25;
-GameHandler.tracks = {};
-GameHandler.notes = {};
-GameHandler.bound = {};
-
---- GetSongs
--- @return Song[]
-function GameHandler:GetSongs()
-	local songInfo = {};
-	for _, song in pairs(SONGS_FOLDER:GetChildren())do
-		local data = require(song);
-		table.insert(
-			songInfo,
-			{
-				Name = song.Name;
-				CoverArt = data.AudioCoverImageAssetId;
-				Difficulty = data.AudioDifficulty;
-			}
-		)
-	end
-	return songInfo;
-end
-
---- SetGameSpeed
+--- constructor
+-- @param track: number
 -- @param speed: number
-function GameHandler:SetGameSpeed(speed)
-	self.speed = speed;
-end
-
---- GetPath
--- @param path: string
-local function GetPath(path)
-	local parent = SONGS_FOLDER;
-	for _, str in pairs(path:split("/"))do
-		parent = parent:WaitForChild(str, 1);
-		if (not parent) then
-			return;
-		end
-	end
-	return parent;
-end
-
---- SetRound
--- @param path: string
-function GameHandler:SetRound(path)
-	--- get the object
-	self.notes = {};
-	--SoundHandler:PlayMusic(0);
-	
-	local parent = GetPath(path);
-	self.CurrentMap = path;
-	
-	--- load bgm
-	local round = require(parent);
-	local beatOffset = round.AudioNotePrebufferTime / 1000;
-	local resolve = SoundHandler:PlayMusic(round.AudioAssetId);
-	self.BackgroundGui.Image = round.AudioCoverImageAssetId;
-
-	--- countdown
-	for countdown = 3, 0, -1 do
-		if (self.CurrentMap ~= path) then
-			return;
-		end
-		self.DisplayText:Fire(countdown);	
-		SoundHandler:PlaySound("countdown");
-		wait(0.75);
-	end
-	
-	--- init
-	self.Add = 0;
-	self.Max = 0;
-	self.timeScale = 0;
-	self.gameTime = 0;
-	
-	--- load map
-	local off = round.AudioTimeOffset;
-	local max = 0;
-	for _, hitObject in pairs(round.HitObjects) do
-		if (hitObject.duration) then
-			self:AddNoteInDelay(hitObject.Track-1, hitObject.Time + off, "Slider", hitObject.Duration);
-		else
-			self:AddNoteInDelay(hitObject.Track-1, hitObject.Time + off, "Note");
-		end
-		max += 1;
-	end
-		
-	self.Max = max;
-	self.timeScale = round.stdtimescale or 1;
-
-	resolve();
-	
-end
-
---- OnTrackPressed
--- @param track: number
-function GameHandler:OnTrackPressed(track)
-	self.tracks[track]:OnTrackCommand();
-end
-
---- OnGuiScaleChanged
-function GameHandler:OnGuiScaleChanged()
-	--- send tracks information
-	local viewport = self.NotesGui.Parent.AbsoluteSize;
-	self.BackgroundGui.Size = UDim2.new(0, viewport.X, 0, viewport.Y);
-	
-	local size = self.NotesGui.AbsoluteSize;
-	local screenHeight = size.Y;
-	local screenWidth = size.X;
-
-	local offset = screenWidth/4;
-	for _, track in pairs(self.tracks)do
-		track:OnGuiScaleChanged(screenHeight, screenWidth, offset);
-	end
-end
-
---- AddTrack
--- @param num: number
--- @return track: Track
-function GameHandler:AddTrack(num, keybind, color)
-	local track = Track:new(num, color);
-	self.tracks[num] = track;
-	self.bound[keybind.Value] = track;
-	
-	track.Score:Connect(function(add)
-		self.Add += add;
-		self.Max += Config.MAX_PTS;
-
-		if (add > 0) then
-			SoundHandler:PlaySound("hit");
-			self.Combo += 1;
-		else
-			SoundHandler:PlaySound("miss");
-			self.Combo = 0;
-		end
-		
-		self.DisplayText:Fire(self.Combo);
-	end)
-end
-
---- SetNoteGui
--- @param to: Frame
-function GameHandler:SetNoteGui(to)
-	--- set important variables
-	Track.Notes = to;
-	self.NotesGui = to;
-	self.BackgroundGui = to:WaitForChild("Background");
-	
-	--- set sound parents
-	SoundHandler:SetParent(to.Parent);
-	
-	--- hook connections
-	self.NotesGui.Changed:Connect(function()
-		self:OnGuiScaleChanged();
-	end)
-end
-
---- AddNoteInDelay
--- Note add dealy lol
--- @param track: number
--- @param ms: number
-function GameHandler:AddNoteInDelay(track, ms, typ, ...)
-	local second = (ms/1000) - (1/self.speed);
-	table.insert(
-		self.notes, 
+function Slider:new(track, speed, distance)
+	return setmetatable(
 		{
-			Type = typ;
-			Time = second; 
-			Track = track;
-			Args = {...};
+			Passed = Signal:new();
+			
+			hitBot = false;
+			
+			posX = track / 4;
+			positionTop = -(1/speed)*(distance/1000); -- every 1 second the thing goes from top to bottom
+			positionBot = 0;
+			track = track;
+			speed = speed;
+		}
+	, Slider);
+end
+
+--- GetPosition
+-- @param origin: number
+-- @return pos: number
+function Slider:GetPosition(origin)
+	return self.hitBot and self.positionTop or self.positionBot;
+end
+
+--- OnAwake
+-- @param MakeSleeve: function
+function Slider:OnAwake(MakeSleeve)
+	local sleeveBot = MakeSleeve(
+		self.trackPos,
+		self.parent,
+		{
+			ImageColor3 = self.color;
 		}
 	);
+	
+	local sleeveTop = MakeSleeve(
+		self.trackPos,
+		self.parent,
+		{
+			ImageColor3 = self.color;
+		}
+	);
+	
+	local sleeveBg = Instance.new("Frame");
+	sleeveBg.Size = UDim2.new(0.25, 0, self.positionTop - self.positionBot, 0);
+	sleeveBg.AnchorPoint = Vector2.new(0, 0.5);
+	sleeveBg.BackgroundColor3 = self.color;
+	sleeveBg.BackgroundTransparency = 0.5;
+	sleeveBg.ZIndex = 50;
+	sleeveBg.BorderSizePixel = 0;
+	sleeveBg.Parent = self.parent;
+	
+	--- insert sleeve data to the note
+	self.SleeveBg = sleeveBg;
+	self.SleeveBottom = sleeveBot;
+	self.SleeveTop = sleeveTop;	
 end
 
---- InsertNote
-function GameHandler:InsertNote()
-	local data = self.notes[1];
-	if (data and data.Time <= self.gameTime) then
-		self.tracks[data.Track]:OnNoteAdded(data.Type, self.speed, unpack(data.Args));
-		table.remove(self.notes, 1);
-		
-		self:InsertNote();
-	end	
+--- OnNoteReleased
+-- @param origin: number 
+-- @param size: number 
+-- @param score: NoteConfig 
+function Slider:OnNoteReleased(origin, size, score)	
+	if (not score) then
+		self.Score:Fire(0);
+		return;
+	end
+	
+	self.Score:Fire(score.pts_add);
+	self.hitBot = false;
+	self:Destroy();
+end
+
+--- OnNoteHeld
+-- @param origin: number
+-- @param size: number
+-- @param score: NoteConfig
+function Slider:OnNoteHeld(origin, size, score)
+	if (self.hitBot) then
+		self.Score:Fire(0);
+		return;
+	end
+	
+	if (not score) then
+		self.Score:Fire(0);
+		return;
+	end
+	
+	self.Score:Fire(score.pts_add);
+	self.hitBot = true;
+end
+
+--- OnNoteHeld
+function Slider:Destroy()
+	self.Passed:Fire(true);
+
+	--- Sweep other objects
+	self.Score = nil;
+
+	self.Passed:Destroy();
+	self.SleeveBottom:Destroy();
+	self.SleeveTop:Destroy();
+	self.SleeveBg:Destroy();
+	
+	--- Sweep all contents
+	self.position = nil;
+	self.track = nil;
+	self.speed = nil;
+	self.color = nil;
+	self.parent = nil;
+	self.trackPos = nil;
 end
 
 --- Update
 -- @param dt: number
-function GameHandler:Update(dt)
-	self:InsertNote();
-	self.gameTime += (dt * self.timeScale);
+function Slider:Update(dt)
+	local delta = (dt * self.speed);
+	self.positionBot += delta;
+	self.positionTop += delta;
 	
-	for _, track in pairs(self.tracks)do
-		track:Update(dt * self.timeScale);
+	self.SleeveBg.Position = UDim2.new(self.posX, 0, (self.positionTop + self.positionBot)/2, 0);
+	self.SleeveBottom.Position = UDim2.new(self.posX, 0, self.positionBot, 0);
+	self.SleeveTop.Position = UDim2.new(self.posX, 0, self.positionTop, 0);
+	
+	if (self.positionTop > Config.NOTE_FAIL_RANGE) then
+		self.Score:Fire(0);
+		self:Destroy();
 	end
+
+	return;
 end
 
---- Bind input
-UserInputService.InputBegan:Connect(function(input, processed)
-	if (processed) then
-		return;
-	end
-
-	if (GameHandler.bound[input.KeyCode.Value]) then
-		GameHandler.bound[input.KeyCode.Value]:OnTrackCommand();
-	end
-end)
-
-UserInputService.InputEnded:Connect(function(input, processed)
-	if (processed) then
-		return;
-	end
-
-	if (GameHandler.bound[input.KeyCode.Value]) then
-		GameHandler.bound[input.KeyCode.Value]:OnTrackReleased();
-	end
-end)
-
---- export
-return GameHandler;
+--- Export
+return Slider;
