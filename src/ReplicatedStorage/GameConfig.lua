4a8b0177fce7d0bf011c1b0362e2de26ba623d57
magicalnoob <61070617+magicalnoob@users.noreply.github.com>
2020-11-10 20:03:51 -0800

    Update GameConfig.lua

--- src/ReplicatedStorage/GameConfig.lua
+++ src/ReplicatedStorage/GameConfig.lua
@@ -7,11 +7,44 @@ GameConfig.__index = GameConfig;
 
 GameConfig.NOTE_RANGE = 1.5;
 GameConfig.NOTE_FAIL_RANGE = 1.15;
-GameConfig.NOTE_SCORES = {
-	perfect = 25;
-	great = 50;
-	meh = 100;
-	bad = 150;
+GameConfig.MAX_TRACKS = 4;
+
+GameConfig.TRACK_SETTINGS = {
+	{
+		Enum.KeyCode.A,
+		BrickColor.new("Bright red")
+	},
+	{
+		Enum.KeyCode.S,
+		BrickColor.new("Bright green")
+	},
+	{
+		Enum.KeyCode.L,
+		BrickColor.new("Bright green")
+	},
+	{
+		Enum.KeyCode.Semicolon,
+		BrickColor.new("Bright red")
+	}	
 };
+
+GameConfig.NOTE_SCORES = {
+	perfect = {
+		range = 25;
+		pts_add = 4;
+	};
+	great = {
+		range = 50;
+		pts_add = 3;
+	};
+	mesh = {
+		range = 75;
+		pts_add = 2;
+	};
+	bad = {
+		range = 100;
+		pts_add = 1;
+	}
+}
 
 return GameConfig;
